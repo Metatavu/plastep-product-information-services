a2ddf891ed4cf1501f96837c860fc4e5530cd205
Antti Leppä <antti.leppa@metatavu.fi>
2023-02-23 19:56:58 +0200

    Cleaned up AbstractApi.kt

--- src/main/kotlin/fi/metatavu/plastep/productinformation/rest/AbstractApi.kt
+++ src/main/kotlin/fi/metatavu/plastep/productinformation/rest/AbstractApi.kt
@@ -1,9 +1,6 @@
-package fi.metatavu.plastep.api.abstracts
+package fi.metatavu.plastep.productinformation.rest
 
-import org.eclipse.microprofile.jwt.JsonWebToken
-import java.util.*
 import javax.enterprise.context.RequestScoped
-import javax.inject.Inject
 import javax.ws.rs.core.Context
 import javax.ws.rs.core.Response
 import javax.ws.rs.core.SecurityContext
@@ -16,33 +13,16 @@ import javax.ws.rs.core.SecurityContext
 @RequestScoped
 abstract class AbstractApi {
 
-    @Inject
-    lateinit var jsonWebToken: JsonWebToken
-
     @Context
     lateinit var securityContext: SecurityContext
 
     /**
-     * Returns logged user id
+     * Checks if user has integration role
      *
-     * @return logged user id
+     * @return if user has integration role
      */
-    protected val loggedUserId: UUID?
-        get() {
-            if (jsonWebToken.subject != null) {
-                return UUID.fromString(jsonWebToken.subject)
-            }
-
-            return null
-        }
-
-    /**
-     * Checks if user is manager
-     *
-     * @return if user is manager
-     */
-    protected fun isManager(): Boolean {
-        return securityContext.isUserInRole("manager")
+    protected fun hasIntegrationRole(): Boolean {
+        return securityContext.isUserInRole("integration")
     }
 
     /**
@@ -59,81 +39,6 @@ abstract class AbstractApi {
     }
 
     /**
-     * Constructs ok response with total count header
-     *
-     * @param entity payload
-     * @return response
-     */
-    protected fun createOk(entity: Any?, count: Long): Response {
-        return Response
-            .status(Response.Status.OK)
-            .entity(entity)
-            .header("Total-Count", count.toString())
-            .header("Access-Control-Expose-Headers", "Total-Count")
-            .build()
-    }
-
-    /**
-     * Constructs ok response and puts count parameter to header
-     *
-     * @param entity payload
-     * @param count Total-Count header
-     * @return response
-     */
-    protected fun createOkCounterHeader(entity: Any, count: Int): Response {
-        return Response
-            .status(Response.Status.OK)
-            .header("Total-Count", count.toString())
-            .entity(entity)
-            .build()
-    }
-
-    /**
-     * Constructs no content response
-     *
-     * @param entity payload
-     * @return response
-     */
-    protected fun createAccepted(entity: Any?): Response {
-        return Response
-            .status(Response.Status.ACCEPTED)
-            .entity(entity)
-            .build()
-    }
-
-    /**
-     * Constructs no content response
-     *
-     * @return response
-     */
-    protected fun createNoContent(): Response {
-        return Response
-            .status(Response.Status.NO_CONTENT)
-            .build()
-    }
-
-    /**
-     * Constructs not modified response
-     *
-     * @return response
-     */
-    protected fun createNotModified(): Response {
-        return Response
-            .status(Response.Status.NOT_MODIFIED)
-            .build()
-    }
-
-    /**
-     * Constructs bad request response
-     *
-     * @param message message
-     * @return response
-     */
-    protected fun createBadRequest(message: String): Response {
-        return createError(Response.Status.BAD_REQUEST, message)
-    }
-
-    /**
      * Constructs not found response
      *
      * @param message message
@@ -141,46 +46,6 @@ abstract class AbstractApi {
      */
     protected fun createNotFound(message: String): Response {
         return createError(Response.Status.NOT_FOUND, message)
-    }
-
-    /**
-     * Constructs not found response
-     *
-     * @return response
-     */
-    protected fun createNotFound(): Response {
-        return Response
-            .status(Response.Status.NOT_FOUND)
-            .build()
-    }
-    /**
-     * Constructs not found response
-     *
-     * @param message message
-     * @return response
-     */
-    protected fun createConflict(message: String): Response {
-        return createError(Response.Status.CONFLICT, message)
-    }
-
-    /**
-     * Constructs not implemented response
-     *
-     * @param message message
-     * @return response
-     */
-    protected fun createNotImplemented(message: String): Response {
-        return createError(Response.Status.NOT_IMPLEMENTED, message)
-    }
-
-    /**
-     * Constructs internal server error response
-     *
-     * @param message message
-     * @return response
-     */
-    protected fun createInternalServerError(message: String): Response {
-        return createError(Response.Status.INTERNAL_SERVER_ERROR, message)
     }
 
     /**
@@ -194,22 +59,12 @@ abstract class AbstractApi {
     }
 
     /**
-     * Constructs unauthorized response
-     *
-     * @param message message
-     * @return response
-     */
-    protected fun createUnauthorized(message: String): Response {
-        return createError(Response.Status.UNAUTHORIZED, message)
-    }
-
-    /**
      * Creates not found response with given parameters
      *
      * @param target target of the find method
      * @param id ID of the target
      */
-    protected fun createNotFoundWithMessage(target: String, id: UUID): Response {
+    protected fun createNotFoundWithMessage(target: String, id: Int): Response {
         return createNotFound("$target with ID $id could not be found")
     }
 
@@ -234,13 +89,6 @@ abstract class AbstractApi {
     }
 
     companion object {
-        const val NO_LOGGED_USER_MESSAGE = "Could not find logged user"
-        const val CUSTOMER_NO_ID = "Customer has no id!"
-        const val NOT_FOUND_MESSAGE = "Not found"
-        const val UNAUTHORIZED = "Unauthorized"
-        const val FORBIDDEN = "Forbidden"
-        const val MISSING_REQUEST_BODY = "Missing request body"
-
         const val PRODUCT = "Product"
     }
 
--- src/main/kotlin/fi/metatavu/plastep/productinformation/system/SystemApiImpl.kt
+++ src/main/kotlin/fi/metatavu/plastep/productinformation/system/SystemApiImpl.kt
@@ -1,6 +1,6 @@
 package fi.metatavu.plastep.productinformation.system
 
-import fi.metatavu.plastep.api.abstracts.AbstractApi
+import fi.metatavu.plastep.productinformation.rest.AbstractApi
 import fi.metatavu.plastep.productinformation.spec.SystemApi
 import javax.ws.rs.Path
 import javax.ws.rs.core.Response
